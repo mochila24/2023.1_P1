f0bff227132d5edfa8f7916045f5fb4712bc4add
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-27 18:34:37 -0300

    atualizado

--- Q8L1.js
+++ Q8L1.js
@@ -1,17 +1,14 @@
 const prompt = require('prompt-sync')();
 
-let nome = ''
-let notas = 0
-let media = 0
-let situa = ''
+let notas = 0;
 
-nome = prompt('Nome do estudante: ')
+const nome = prompt('Nome do estudante: ')
 for (let c=0; c<4; c++){
     notas += Number(prompt(`Nota ${c + 1}: `))
 }
 
-media = notas/4
+const media = notas / 4
 
-situa = media >= 6 ? 'Aprovado' : ( media < 4 ? 'Reprovado' : 'Recuperação')
+const situa = media >= 6 ? 'Aprovado' : ( media < 4 ? 'Reprovado' : 'Recuperação')
 
 console.log(`${nome} obteve média: ${media.toFixed(2)}. Status: ${situa}.`)
