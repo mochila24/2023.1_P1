69b1e7ba25d34be3f8179d9504eca0fa3adf3563
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-27 12:51:25 -0300

    conserto de código pelo chatgpt

--- Q6L1.js
+++ Q6L1.js
@@ -1,13 +1,17 @@
-prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-let nome1 = ''
-let id1 = 0
-let nome2 = ''
-let id2 = 0
-let nome3 = ''
-let id3 = 0
+let nome1 = '';
+let id1 = 0;
+let nome2 = '';
+let id2 = 0;
+let nome3 = '';
+let id3 = 0;
 
-nome1 = prompt('Bote um nome: ')
-id1 = Number(prompt(`Qual a idade de ${nome1}? `))
-nome2 = prompt('Nome para uma segunda pessoa fictícia: ')
-console.log(`nome: ${nome1} idade: ${id1} nome2: ${nome2}`)
+nome1 = prompt('Digite um nome: ');
+id1 = Number(prompt(`Qual a idade de ${nome1}? `));
+nome2 = prompt('Digite o nome de uma segunda pessoa fictícia: ');
+console.log(`nome: ${nome1} idade: ${id1} nome2: ${nome2}`);
+
+
+
+
