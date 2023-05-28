e244334e3b2f9dea30d10f7392ed8e0c0a43fa90
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-28 09:49:04 -0300

    formatação de valor em reais

--- Q10L1.js
+++ Q10L1.js
@@ -8,4 +8,8 @@ let cuscon;
 cusfab = Number(prompt('Informe o custo de fábrica do carro: '))
 cuscon = (cusfab * dist) + (cusfab * imp) + cusfab
 
-console.log(`O valor de venda do carro é de R$${cuscon.toFixed(2)}.`)
+// Formatação do valor final em reais
+
+const valorFormatado = cuscon.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+console.log(`O valor de venda do carro é de ${valorFormatado}.`)
