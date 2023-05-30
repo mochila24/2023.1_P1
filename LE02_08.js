38226cb4939531b8a33a335613945d08f7cd852e
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-30 17:25:49 -0300

    entrada pelo teclado

--- LE02_08.js
+++ LE02_08.js
@@ -1,13 +1,17 @@
-let p = 1
-let s = 0
-let n = 4
-let m = 4
-let t = 0
-let cont = 0
+const prompt = require('prompt-sync')();
+
+let n = Number(prompt('Coloque um número: '));
+let m = Number(prompt('Informe outro número: '));
+
+
+let p = 1;
+let s = 0;
+let t = 0;
+let cont = 0;
 while (cont < n){
-    cont += 1
-    s += cont
-    p *= cont
+    cont += 1;
+    s += cont;
+    p *= cont;
 }
-t = s * p
-console.log("O produto de ",s," por ",p," é ",t)
+t = s * p;
+console.log("O produto de "+ s +" por "+ p +" é " + t);
