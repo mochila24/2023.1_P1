07899594e7b676e0c0e8b34fdcb380a3a6e02f0b
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-30 17:29:00 -0300

    entrada pelo teclado
    logaritmos neperianos

--- LE02_09.js
+++ LE02_09.js
@@ -1,13 +1,17 @@
-let f = 1
-let n = 4
-let cont = 0
-let ln = 0
-while (cont < n){
-    cont += 1
-    f *= cont
-    ln += 1/f
-    console.log("Fatorial: ",f)
-    console.log("LN: ",ln)
+const prompt = require('prompt-sync')();
+
+let n = Number(prompt('Digite o valor de N: '));
+let f = 1;
+let cont = 0;
+let ln = 0;
+
+while (cont < n) {
+    cont += 1;
+    f *= cont;
+    ln += 1 / f;
+    console.log("Fatorial: ", f);
+    console.log("LN: ", ln);
 }
-console.log("Fatorial: ",f)
-console.log("Logaritmo neperiano de ",n," é ",1+ln)
+
+console.log("Fatorial: ", f);
+console.log("Logaritmo neperiano de ", n, " é ", 1 + ln);
