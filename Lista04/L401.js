241f36671952329dc55a8935fa4296d873a4e0c9
Brunito <126920514+mochila24@users.noreply.github.com>
2023-06-03 17:29:27 -0300

    funcao Delta

---
+++ Lista04/L401.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+let delta = 0;
+let naturezaDaRaiz;
+
+console.log('A partir da expressão ax² + bx + c = 0, identifique os valores de: ');
+let a = Number(prompt('a = '));
+let b = Number(prompt('b = '));
+let c = Number(prompt('c = '));
+let resultado = 0;
+
+console.clear()
+
+function calcularDelta(a, b, c){
+    delta = b*b - 4 * a * c;
+    return delta
+}
+
+resultado = calcularDelta(a, b, c);
+if (resultado > 0) {
+    naturezaDaRaiz = 'A equação possui duas raízes reais distintas.'
+} else if (resultado === 0) {
+    naturezaDaRaiz = 'A equação possui duas raízes reais e iguais.'
+} else {
+    naturezaDaRaiz = 'A equação não possui raízes reais.'
+}
+
+console.log(`valor de Delta: ${resultado}. ${naturezaDaRaiz}`);
