911dff1d96d42ead3e35a587cdc7a0e7c2d96a59
Brunito <126920514+mochila24@users.noreply.github.com>
2023-06-20 21:37:16 -0300

    melhoria usando apenas dois digitos

--- objClasse01.js
+++ objClasse01.js
@@ -22,4 +22,48 @@ class Idade {
       return this._idade;
     }
   }
+
   
+/*
+
+usado para usar apenas dois digitos para indicar o ano
+
+
+  class Idade {
+    constructor(anoNascimento) {
+      this._anoNascimento = anoNascimento;
+      this._anoAtual = new Date().getFullYear();
+      this._idade = this._anoAtual - this._anoNascimento;
+    }
+  
+    setAnoAtual(ano) {
+      if (ano >= 0 && ano <= 99) {
+        const anoAtualCompleto = new Date().getFullYear();
+        const seculoAtual = Math.floor(anoAtualCompleto / 100) * 100; // Obtém o século atual
+        this._anoAtual = seculoAtual + ano;
+        this._idade = this._anoAtual - this._anoNascimento;
+      } else {
+        console.log("Ano inválido!");
+      }
+    }
+  
+    setAnoNascimento(ano) {
+      if (ano >= 0 && ano <= 99) {
+        const anoAtualCompleto = new Date().getFullYear();
+        const seculoAtual = Math.floor(anoAtualCompleto / 100) * 100; // Obtém o século atual
+        const seculoAnterior = seculoAtual - 100; // Obtém o século anterior
+        const seculo = (anoAtualCompleto - ano >= 0) ? seculoAtual : seculoAnterior; // Determina o século com base no ano atual
+        this._anoNascimento = seculo + ano;
+        this._idade = this._anoAtual - this._anoNascimento;
+      } else {
+        console.log("Ano inválido!");
+      }
+    }
+  
+    calcularIdade() {
+      return this._idade;
+    }
+  }
+  
+*/
+
